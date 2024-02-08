ca30d6cfe5411d49a0e36fd137f3feab14fb1c5e
X6STRIVE <X6STRIVE@IN05N001NS>
2024-02-08 15:56:23 +0530

    fixed stuff

---
+++ IEventAggregagtorLib/IEventAggregator.cs
@@ -0,0 +1,16 @@
+﻿using AutomaticClimateControlSystem;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticClimateControlSystem
+{
+    public interface IEventAggregator
+    {
+        public void Publish(object @event);
+        public void Subscribe(Type eventType, ISubscriber subscriber);
+    }
+}
---
+++ TemperatureThresholdSensorLib/TempThresholdExceededEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticClimateControlSystem
+{
+    public class TempThresholdExceededEvent(float currentTemp)
+    {
+        public float CurrentTemp = currentTemp;
+        // public float thresholdTemp = thresholdTemp;
+    }
+}
