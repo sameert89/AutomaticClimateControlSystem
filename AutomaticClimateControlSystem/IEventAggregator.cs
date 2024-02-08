1e8547d2b28f5214fd138fbaebe7e965644dd8e5
X6STRIVE <X6STRIVE@IN05N001NS>
2024-02-08 14:47:26 +0530

    Added Climate Event Handlers

--- AutomaticClimateControlSystem/IEventAggregator.cs
+++ AutomaticClimateControlSystem/IEventAggregator.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AutomaticClimateControlSystem;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,3 +14,17 @@ namespace AutomaticClimateControlSystem
         public void Subscribe(Type eventType, ISubscriber subscriber);
     }
 }
+
+// adapter
+IEventAggregator eventAggregator;
+Adapter`(IEventAggregator eventAggregator){
+    this.eventAggregator = eventAggregator;
+    eventAggregator.Subscribe(PassengerCountChangeEvent,this)
+    eventAggregator.Subscribe(TempThresholdExceededEvent, this)
+
+    calculator object
+
+    dictionary <Type, Action<object>>()
+
+
+    Event Aggregator -> sent event -> Adapter -> gets type of event -> Action<object> // event object -> 
